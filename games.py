53f3de92e641ccfbc2742c938da4c9956d378fc6
Thomas Buck <thomas@venom.fritz.box>
2023-08-18 13:12:11 +0200

    add telegram test

---
+++ games.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <xythobuz@xythobuz.de> wrote this file.  As long as you retain this notice
+# you can do whatever you want with this stuff. If we meet some day, and you
+# think this stuff is worth it, you can buy me a beer in return.   Thomas Buck
+# ----------------------------------------------------------------------------
+
+camp_pink = (251, 72, 196)
+camp_green = (63, 255, 33)
+
+from splash import SplashScreen
+from scroll import ScrollText
+from solid import Solid
+from life import GameOfLife
+from net import CheckHTTP
+from image import ImageScreen
+from qr import QRScreen
+from snake import Snake
+from gamepad import InputWrapper
+from manager import Manager
+from tetris import Tetris
+import util
+
+url_uba = "http://ubabot.frubar.net"
+url_printer = "http://i3-am8.fritz.box"
+
+scroll_speed = 50
+
+# Need to import InputWrapper before initializing RGB Matrix on Pi
+i = util.getInput()
+t = util.getTarget(i)
+
+# Loading fonts and graphics takes a while.
+# So show a splash screen while the user waits.
+splash = SplashScreen(t)
+t.loop_start()
+splash.draw()
+t.loop_end()
+
+# Main "Menu"
+m = Manager(t, i)
+m.add(GameOfLife(t, 20, (0, 255, 0), (0, 0, 0), None, 2.0))
+m.add(Solid(t, 1.0))
+m.add(Tetris(t, i,))
+m.add(Solid(t, 1.0))
+m.add(Snake(t, i, camp_pink, camp_green))
+m.add(Solid(t, 1.0))
+
+m.restart()
+util.loop(t, m.draw)
---
+++ telegram.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <xythobuz@xythobuz.de> wrote this file.  As long as you retain this notice
+# you can do whatever you want with this stuff. If we meet some day, and you
+# think this stuff is worth it, you can buy me a beer in return.   Thomas Buck
+# ----------------------------------------------------------------------------
+
+import util
+import time
+import sys
+import json
+from config import Config
+
+class TelegramBot:
+    def __init__(self):
+        if (Config.telegram_key == None) or len(Config.telegram_key) < 10:
+            raise RuntimeError("No Telegram Bot API Key set!")
+
+        self.get = None
+        self.post = None
+        self.previous_offset = None
+        self.api = "https://api.telegram.org/bot" + Config.telegram_key + "/"
+
+    def message(self, text):
+        for chat in Config.telegram_whitelist:
+            data = {
+                "chat_id": chat,
+                "text": text,
+            }
+            r = self.fetch(self.api + "sendMessage", data)
+            print(r)
+
+    def poll(self):
+        if self.previous_offset != None:
+            data = {
+                "offset": self.previous_offset + 1,
+            }
+            r = self.fetch(self.api + "getUpdates", data)
+        else:
+            r = self.fetch(self.api + "getUpdates")
+
+        print(r)
+
+        for msg in r["result"]:
+            if (self.previous_offset == None) or (self.previous_offset < msg["update_id"]):
+                self.previous_offset = msg["update_id"]
+
+    def fetch(self, url, data = None):
+        # lazily initialize WiFi
+        if self.get == None:
+            self.get, self.post = util.getRequests()
+            if self.get == None:
+                return None
+
+        try:
+            if data == None:
+                print("GET " + url)
+                r = self.get(url)
+            else:
+                js = json.dumps(data)
+                print("POST " + url + " " + js)
+                r = self.post(url, headers = { "Content-Type": "application/json" }, data = js)
+
+            # explitic close on Response object not needed,
+            # handled internally by r.content / r.text / r.json()
+            # to avoid this automatic behaviour, first access r.content
+            # to trigger caching it in response object, then close
+            # socket.
+            tmp = r.content
+            if hasattr(r, "raw"):
+                if r.raw != None:
+                    r.raw.close()
+                    r.raw = None
+
+            return r.json()
+        except Exception as e:
+            print()
+            print(url)
+            if hasattr(sys, "print_exception"):
+                sys.print_exception(e)
+            else:
+                print(e)
+            print()
+            return None
+
+if True:#__name__ == "__main__":
+    b = TelegramBot()
+    b.message("Hello World!")
+    while True:
+        b.poll()
